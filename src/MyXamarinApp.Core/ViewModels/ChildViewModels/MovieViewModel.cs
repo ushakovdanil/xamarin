1292cf3309cd8d13690e87130e163d2f1c9dcf49
Danil Ushakov <ushakovdanil77@gmail.com>
2023-04-17 14:18:39 +0300

    feat(frame.app): add movie items and fragments

---
+++ src/MyXamarinApp.Core/ViewModels/ChildViewModels/MovieViewModel.cs
@@ -0,0 +1,29 @@
+﻿using MvvmCross.Commands;
+using MvvmCross.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyXamarinApp.Core.ViewModels.ChildViewModels
+{
+    public class MovieViewModel: MvxViewModel
+    {
+        private Movie _currentMovie;
+        public Movie CurrentMovie
+        {
+            get => _currentMovie;
+            set
+            {
+                _currentMovie = value;
+                RaiseAllPropertiesChanged();
+            }
+        }
+
+        public Guid Id => CurrentMovie.Id;
+        public string Title => CurrentMovie.Title;
+
+        public string Description => CurrentMovie.Description;
+        public int MyProperty { get; set; }
+        public IMvxCommand AddToSavedClassCommand { get; set; }
+    }
+}
