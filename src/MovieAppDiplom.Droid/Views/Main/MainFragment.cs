d2c8dc4ab2156f2cd1d052f497a2a05c4e414641
Danil Ushakov <ushakovdanil77@gmail.com>
2023-04-22 14:54:52 +0300

    add layout manager

--- src/MovieAppDiplom.Droid/Views/Main/MainFragment.cs
+++ src/MovieAppDiplom.Droid/Views/Main/MainFragment.cs
@@ -9,14 +9,23 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using AndroidX.RecyclerView.Widget;
 using MovieAppDiplom.Core.ViewModels.Main;
 using MvvmCross.Platforms.Android.Presenters.Attributes;
 
 namespace MovieAppDiplom.Droid.Views.Main
 {
     [MvxFragmentPresentation(typeof(MainContainerViewModel), Resource.Id.main_content)]
-    public class MainFragment : BaseFragment<TableMoviesViewModel>
+    public class TableMoviesFragment : BaseFragment<TableMoviesViewModel>
     {
         protected override int FragmentLayoutId => Resource.Layout.table_movies;
+
+        public override void OnViewCreated(View view, Bundle savedInstanceState)
+        {
+            base.OnViewCreated(view, savedInstanceState);
+            var recycler = view.FindViewById<RecyclerView>(Resource.Id.movies_recycler);
+            recycler.SetLayoutManager(new GridLayoutManager(Application.Context, spanCount: 6));
+            recycler.HasFixedSize = false;
+        }
     }
 }
